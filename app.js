26c11157d76890b4f86add21d7981fad27283343
Saroj Niraula <sarojniraula@Sarojs-MacBook-Pro.local>
2018-08-07 15:12:55 +0545

    Hackerbay week 1

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// view engine 
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hacker Bay'});
+});
+
+router.get('/test/:id', function(req, res, next) {
+  res.render('test',{ output: req.params.id });
+});
+
+router.post('/test/submit', function(req, res, next) {
+  id = req.body.id;
+  res.redirect('/test/' + id);
 });
 
 module.exports = router;
